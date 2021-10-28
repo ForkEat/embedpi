775bdaaebacd50f37093ac7c416d27e85e521fed
Thomas LACAZE <contact@thomaslacaze.fr>
2021-10-28 16:37:36 +0200

    add txwifi into embed package

---
+++ config/config.go
@@ -0,0 +1,23 @@
+package config
+
+import "os"
+
+// getEnv gets an environment variable or sets a default if
+// one does not exist.
+func GetEnv(key, fallback string) string {
+	value := os.Getenv(key)
+	if len(value) == 0 {
+		return fallback
+	}
+
+	return value
+}
+
+// setEnvIfEmp<ty sets an environment variable to itself or
+// fallback if empty.
+func SetEnvIfEmpty(env string, fallback string) (envVal string) {
+	envVal = GetEnv(env, fallback)
+	os.Setenv(env, envVal)
+
+	return envVal
+}
--- go.mod
+++ go.mod
@@ -3,7 +3,13 @@ module main
 go 1.17
 
 require (
+	github.com/bhoriuchi/go-bunyan v0.0.0-20170831222709-8815b5fdce8c
 	github.com/bieber/barcode v0.0.0-20201127170204-1d90414c63eb
 	github.com/google/uuid v1.3.0
+	github.com/gorilla/handlers v1.5.1
+	github.com/gorilla/mux v1.8.0
+	github.com/txn2/txwifi v1.0.4
 	gocv.io/x/gocv v0.28.0
 )
+
+require github.com/felixge/httpsnoop v1.0.2 // indirect
